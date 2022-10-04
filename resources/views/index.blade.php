50864ff6fd403399604941ac93555ed510995ece
ilham50402 <ilham50402@gmail.com>
2022-10-04 11:59:24 +0700

    Project Petani Udah

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,39 +2,33 @@
 
 @section('container')
 <div class="row">
-    <!-- section one -->
-    <div class="col text-center">
-        <section class="one">
-            <h1>INI SLIDER</h1>
-        </section>
+       <!-- Section 1 -->
+       <div class="row section1">
+      <div class="col-6">
+        <img src="img/Nuswantara.jpg" alt="section1" class="img-fluid">
+      </div>
+      <div class="col-5">
+        <h3> Nuswantara Garden</h3>
+        <p>Maju untuk Petani Atsiri</p>
+      </div>
+      </div>
     </div>
-    <!-- end section one -->
 
-    <!-- section two -->
-    <div class="col text-center">
-        <section class="two">
-            <card class="card" style="width: 18rem">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        Special title treatment
-                    </h5>
-                    <p class="card-text">
-                        With supporting text below as a natural
-                        lead-in to additional content.
-                    </p>
-                    <a
-                        href="/login-user"
-                        class="btn btn-primary"
-                        >DONASI</a
-                    >
-                    <a
-                        href="/login-petani"
-                        class="btn btn-primary"
-                        >GALANG DANA</a
-                    >
-                </div>
-            </card>
-        </section>
+    <!-- Section 2 -->
+    <div class="row section2">
+      <div class="col-6">
+        <img src="img/donasi.jpg" alt="section1" class="img-fluid" style="padding bottom : 20px;">
+        <h3> DONATUR</h3>
+        <p>Mari Berbagi Untuk Kejayaan Petani Negeri</p>
+        <a href="/blog/berita2/" class="btn btn-primary"> More Info</a>
+      </div>
+      <div class="col-6">
+        <img src="img/petani.jpg" alt="section1" class="img-fluid">
+        <h3> PETANI</h3>
+        <p>Berikan project pertanian terbaikmu !!!</p>
+        <a href="/blog/berita3/" class="btn btn-primary"> More Info</a>
+      </div>
+      </div>
     </div>
     <!-- end section two -->
 </div>
